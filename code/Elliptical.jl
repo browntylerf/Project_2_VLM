a3055a198dac04cf96dab7b768074b7e54903849
Ty <browntylerf@gmail.com>
2025-03-14 15:19:57 -0600

    added code folder

---
+++ code/Elliptical.jl
@@ -0,0 +1,71 @@
+using VortexLattice
+using Plots
+
+
+root_chord = 2.2
+span = 15
+ns = 15
+
+function define_elliptical(root_chord, span, ns)
+    chord_distribution(y) = root_chord * sqrt(1-(y/(span/2))^2) #defines chord distribution by modification of ellipse equation
+
+    yle = range(0, stop=span/2, length=ns) #defines y component of wing as a range from 0 to half the span, divided into ns sections
+    xle = zeros(ns) #defines x component of wing as a vector of zeros, same length as ns
+    zle = zeros(ns) #defines z component of wing as a vector of zeros, same length as ns
+    chord = [chord_distribution(y) for y in yle]   #defines chord length at each section by applying chord_distribution function to each element in yle vector
+    
+    return yle, xle, zle, chord #returns these values to be used in VLM calculations
+end
+
+
+function elliptical(root_chord, span, ns)
+    yle, xle, zle, chord = define_elliptical(root_chord, span, ns)
+    theta = fill(2.0*pi/180, ns)
+    phi = fill(0.0, ns)
+    fc = fill((xc) -> 0, ns) #camberline function for each section
+
+    
+    nc = 6
+    spacing_s = Uniform()
+    spacing_c = Uniform()
+    mirror = true
+    symmetric = false
+
+    Sref = 30.0
+    cref = 2.0
+    bref = 15.0
+    rref = [0.50, 0.0, 0.0]
+    Vinf = 1.0
+    ref = Reference(Sref, cref, bref, rref, Vinf)
+
+    alpha = 1.0*pi/180
+    beta = 0.0
+    Omega = [0.0; 0.0; 0.0]
+    fs = Freestream(Vinf, alpha, beta, Omega)
+
+    # construct surface
+    grid, ratio = wing_to_grid(xle, yle, zle, chord, theta, phi, ns, nc;
+        fc = fc, spacing_s=spacing_s, spacing_c=spacing_c, mirror=mirror)
+
+    # create vector containing all grids and ratios
+    grids = [grid]
+    ratios = [ratio]
+
+    # create the system object
+    system = System(grids; ratios)
+
+    # perform steady state analysis
+    steady_analysis!(system, ref, fs; symmetric=symmetric)
+
+    # retrieve near-field forces
+    CF, CM = body_forces(system; frame=Wind())
+
+    # perform far-field analysis
+    CDiff = far_field_drag(system)
+
+    CD, CY, CL = CF
+    Cl, Cm, Cn = CM
+    write_vtk("elliptical-wing", system)
+end
+
+elliptical(root_chord, span, ns)
---
+++ code/Varying_Alpha.jl
@@ -0,0 +1,65 @@
+using VortexLattice
+using Plots
+
+function varying_alpha()
+    xle = [0.0, 0.4]
+    yle = [0.0, 7.5]
+    zle = [0.0, 0.0]
+    chord = [2.2, 1.8]
+    theta = [2.0*pi/180, 2.0*pi/180]
+    phi = [0.0, 0.0]
+    fc = fill((xc) -> 0, 2) #camberline function for each section
+
+    ns = 12
+    nc = 6
+    spacing_s = Uniform()
+    spacing_c = Uniform()
+    mirror = false
+    symmetric = true
+
+    Sref = 30.0
+    cref = 2.0
+    bref = 15.0
+    rref = [0.50, 0.0, 0.0]
+    Vinf = 1.0
+    ref = Reference(Sref, cref, bref, rref, Vinf)
+
+    alpha_range = -10:0.5:10
+    alphas = alpha_range .*pi/180
+    beta = 0.0
+    Omega = [0.0; 0.0; 0.0]
+
+    CL_values = []
+
+    for alpha in alphas
+        fs = Freestream(Vinf, alpha, beta, Omega)
+
+        # construct surface
+        grid, ratio = wing_to_grid(xle, yle, zle, chord, theta, phi, ns, nc;
+        fc = fc, spacing_s=spacing_s, spacing_c=spacing_c)
+
+        # create vector containing all grids and ratios
+        grids = [grid]
+        ratios = [ratio]
+
+     # create the system object
+        system = System(grids; ratios)
+
+        # perform steady state analysis
+        steady_analysis!(system, ref, fs; symmetric=symmetric)
+
+        # retrieve near-field forces
+        CF, CM = body_forces(system; frame=Wind())
+
+        # perform far-field analysis
+        CDiff = far_field_drag(system)
+
+        CD, CY, CL = CF
+        Cl, Cm, Cn = CM
+        push!(CL_values, CL)
+    end
+    plot(alpha_range, CL_values, xlabel="Alpha (degrees)", ylabel="CL", legend=false)
+    savefig("Varying_Alpha.png")
+end
+
+varying_alpha()
