a305e48eef8c6d7a392ae7bd46005de60a65079e
Ty <browntylerf@gmail.com>
2025-03-19 10:48:01 -0600

    update stability_derivatives code

--- code/Stability_Derivatives.jl
+++ code/Stability_Derivatives.jl
@@ -2,14 +2,14 @@ using VortexLattice
 using Plots
 
 function volume_ratios()
-    # wing
+    # wing, kept constant
     xle = [0.0, 0.0]
     yle = [0.0, 5.0]
     zle = [0.0, 0.0]
     chord = [1.0, 1.0]
     theta = [0.0, 0.0]
     phi = [0.0, 0.0]
-    fc = fill((xc) -> 0, 2) # camberline function for each section
+    fc = fill((xc) -> 0, 2)
     ns = 12
     nc = 6
     spacing_s = Uniform()
@@ -31,7 +31,8 @@ function volume_ratios()
     span_h_range = range(1.0, 3.0, 10)
     span_v_range = range(1.0, 3.0, 10)
 
-    horiz_coeff = Float64[]
+    # initialize arrays to store results
+    horiz_coeff = Float64[] 
     vert_coeff = Float64[]
     Cla_horiz = Float64[]
     Cma_horiz = Float64[]
@@ -40,12 +41,12 @@ function volume_ratios()
     Cma_vert = Float64[]
     Cna_vert = Float64[]
 
-    for span_h in span_h_range
+    for span_h in span_h_range #vary horizontal tail span, but keep vertical tail span constant
         # horizontal stabilizer
         xle_h = [0.0, 0.0]
         yle_h = [0.0, span_h]
         zle_h = [0.0, 0.0]
-        chord_h = [0.7, 0.7]
+        chord_h = [1.0, 1.0]
         theta_h = [0.0, 0.0]
         phi_h = [0.0, 0.0]
         fc_h = fill((xc) -> 0, 2) # camberline function for each section
@@ -60,7 +61,7 @@ function volume_ratios()
         xle_v = [0.0, 0.0]
         yle_v = [0.0, 0.0]
         zle_v = [0.0, 1.0]
-        chord_v = [0.7, 0.7]
+        chord_v = [1.0, 1.0]
         theta_v = [0.0, 0.0]
         phi_v = [0.0, 0.0]
         fc_v = fill((xc) -> 0, 2) # camberline function for each section
@@ -72,21 +73,17 @@ function volume_ratios()
 
         symmetric = [false, false, false]
 
-        # generate surface panels for wing
         wgrid, wratio = wing_to_grid(xle, yle, zle, chord, theta, phi, ns, nc;
             mirror=mirror, fc=fc, spacing_s=spacing_s, spacing_c=spacing_c)
-    
-        # generate surface panels for horizontal tail
+
         hgrid, hratio = wing_to_grid(xle_h, yle_h, zle_h, chord_h, theta_h, phi_h, ns_h, nc_h;
             mirror=mirror_h, fc=fc_h, spacing_s=spacing_s_h, spacing_c=spacing_c_h)
         VortexLattice.translate!(hgrid, [4.0, 0.0, 0.0])
     
-        # generate surface panels for vertical tail
         vgrid, vratio = wing_to_grid(xle_v, yle_v, zle_v, chord_v, theta_v, phi_v, ns_v, nc_v;
             mirror=mirror_v, fc=fc_v, spacing_s=spacing_s_v, spacing_c=spacing_c_v)
         VortexLattice.translate!(vgrid, [4.0, 0.0, 0.0])
     
-        # now set normal vectors manually
         ncp = avl_normal_vector([xle[2]-xle[1], yle[2]-yle[1], zle[2]-zle[1]], 0.0*pi/180)
     
         grids = [wgrid, hgrid, vgrid]
@@ -95,7 +92,6 @@ function volume_ratios()
     
         system = System(grids; ratios)
     
-        # overwrite normal vector for each wing panel
         for i = 1:length(system.surfaces[1])
             system.surfaces[1][i] = set_normal(system.surfaces[1][i], ncp)
         end
@@ -105,10 +101,7 @@ function volume_ratios()
         CF, CM = body_forces(system; frame=Stability())
     
         CDiff = far_field_drag(system)
-    
-        CD, CY, CL = CF
-        Cl, Cm, Cn = CM
-    
+
         dCF, dCM = stability_derivatives(system)
         
         Cla, Cma, Cna = dCM[:alpha]
@@ -124,13 +117,12 @@ function volume_ratios()
         push!(Cna_horiz, Clb) #roll stability
     end
 
-
-    for span_v in span_v_range
+    for span_v in span_v_range #vary vertical tail span, but keep horizontal tail span constant
         # horizontal stabilizer
         xle_h = [0.0, 0.0]
         yle_h = [0.0, 1.0]
         zle_h = [0.0, 0.0]
-        chord_h = [0.7, 0.7]
+        chord_h = [1.0, 1.0]
         theta_h = [0.0, 0.0]
         phi_h = [0.0, 0.0]
         fc_h = fill((xc) -> 0, 2) # camberline function for each section
@@ -145,7 +137,7 @@ function volume_ratios()
         xle_v = [0.0, 0.0]
         yle_v = [0.0, 0.0]
         zle_v = [0.0, span_v]
-        chord_v = [0.7, 0.7]
+        chord_v = [1.0, 1.0]
         theta_v = [0.0, 0.0]
         phi_v = [0.0, 0.0]
         fc_v = fill((xc) -> 0, 2) # camberline function for each section
@@ -157,21 +149,17 @@ function volume_ratios()
 
         symmetric = [false, false, false]
 
-        # generate surface panels for wing
         wgrid, wratio = wing_to_grid(xle, yle, zle, chord, theta, phi, ns, nc;
             mirror=mirror, fc=fc, spacing_s=spacing_s, spacing_c=spacing_c)
     
-        # generate surface panels for horizontal tail
         hgrid, hratio = wing_to_grid(xle_h, yle_h, zle_h, chord_h, theta_h, phi_h, ns_h, nc_h;
             mirror=mirror_h, fc=fc_h, spacing_s=spacing_s_h, spacing_c=spacing_c_h)
         VortexLattice.translate!(hgrid, [4.0, 0.0, 0.0])
     
-        # generate surface panels for vertical tail
         vgrid, vratio = wing_to_grid(xle_v, yle_v, zle_v, chord_v, theta_v, phi_v, ns_v, nc_v;
             mirror=mirror_v, fc=fc_v, spacing_s=spacing_s_v, spacing_c=spacing_c_v)
         VortexLattice.translate!(vgrid, [4.0, 0.0, 0.0])
     
-        # now set normal vectors manually
         ncp = avl_normal_vector([xle[2]-xle[1], yle[2]-yle[1], zle[2]-zle[1]], 0.0*pi/180)
     
         grids = [wgrid, hgrid, vgrid]
@@ -180,7 +168,6 @@ function volume_ratios()
     
         system = System(grids; ratios)
     
-        # overwrite normal vector for each wing panel
         for i = 1:length(system.surfaces[1])
             system.surfaces[1][i] = set_normal(system.surfaces[1][i], ncp)
         end
@@ -191,14 +178,10 @@ function volume_ratios()
     
         CDiff = far_field_drag(system)
     
-        CD, CY, CL = CF
-        Cl, Cm, Cn = CM
-    
         dCF, dCM = stability_derivatives(system)
         
         Cla, Cma, Cna = dCM[:alpha]
         Clb, Cmb, Cnb = dCM[:beta]
-        
 
         write_vtk("wing-tail", system)
 
@@ -209,12 +192,20 @@ function volume_ratios()
         push!(Cma_vert, Cnb) #yaw stability
         push!(Cna_vert, Clb) #roll stability
     end
-    print(Cma_vert)
-end
+    plot1 = plot(horiz_coeff, Cla_horiz, xlabel="Horizontal Tail Volume Coefficient", ylabel="Cla", legend=false)
+    plot2 = plot(horiz_coeff, Cma_horiz, xlabel="Horizontal Tail Volume Coefficient", ylabel="Cma", legend=false)
+    plot3 = plot(horiz_coeff, Cna_horiz, xlabel="Horizontal Tail Volume Coefficient", ylabel="Cna", legend=false)
+    plot4 = plot(vert_coeff, Cla_vert, xlabel="Vertical Tail Volume Coefficient", ylabel="Cla", legend=false)
+    plot5 = plot(vert_coeff, Cma_vert, xlabel="Vertical Tail Volume Coefficient", ylabel="Cma", legend=false)
+    plot6 = plot(vert_coeff, Cna_vert, xlabel="Vertical Tail Volume Coefficient", ylabel="Cna", legend=false)
 
-function avl_normal_vector(vector, angle)
-    normal_vector = [vector[1] * cos(angle), vector[2] * cos(angle), vector[3] * sin(angle)]
-    return normal_vector
+    # Display plots
+    display(plot1)
+    display(plot2)
+    display(plot3)
+    display(plot4)
+    display(plot5)
+    display(plot6)
 end
 
 volume_ratios()
